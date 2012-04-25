4f8381d7e95da322f131806c49ef69145dec729e
lstratman <lstratman@gmail.com>
2012-04-25 16:37:05 -0400

    Added new TimeTextBox control.

---
+++ TimeTextBox.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+
+namespace Dojo.Net
+{
+	public class TimeTextBox : DojoInputControl<TimeSpan?>
+	{
+		protected override string DojoType
+		{
+			get
+			{
+				return "dijit.form.TimeTextBox";
+			}
+		}
+
+		public override TimeSpan? Value
+		{
+			get
+			{
+				if (Attributes["value"] == null)
+					return null;
+
+				return TimeSpan.Parse(Attributes["value"].Substring(1));
+			}
+
+			set
+			{
+				if (value == null)
+					Attributes["value"] = null;
+
+				else
+					Attributes["value"] = "T" + value.Value.ToString("hh:mm:ss");
+			}
+		}
+
+		public bool Required
+		{
+			get
+			{
+				return Attributes["required"] != null && bool.Parse(Attributes["required"]);
+			}
+
+			set
+			{
+				Attributes["required"] = value.ToString().ToLower();
+			}
+		}
+	}
+}
